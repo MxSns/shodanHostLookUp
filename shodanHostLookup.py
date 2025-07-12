4e47244f8a6585559411f68b4e09f19f4be099b2
MxSns <maxime.sn210@gmail.com>
2025-07-12 16:32:57 +0200

    update argparse

--- shodanHostLookup.py
+++ shodanHostLookup.py
@@ -4,6 +4,7 @@ parser = argparse.ArgumentParser(prog='shodanHostLookup', description='This scri
 
 parser.add_argument('host', help='The IP address of the target')
 parser.add_argument('-k', '--key', required=True, help='Shodan\'s API key')
+parser.add_argument('-f', '--format', help='Format to display the datas text / json')
 
 args = parser.parse_args()
 
@@ -13,14 +14,26 @@ api_key = (args.key)
 
 # Lookup an IP using shodan's REST API
 response = requests.get(f'https://api.shodan.io/shodan/host/{args.host}?key={api_key}')
-print(response.text)
+
 
 # The response text type is a string, so try to convert to dictionary
-host_info_dictionary = json.loads(response.text)
+hinfd = json.loads(response.text)
 
 # Format the output (indent, sort keys and don't convert Unicode) for printing JSON
-host_info_json = json.dumps(host_info_dictionary, indent=4, sort_keys=True, ensure_ascii=False)
-print(host_info_json)
+host_info_json = json.dumps(hinfd, indent=4, sort_keys=True, ensure_ascii=False)
+# print(host_info_json)
 
 # Print the hostname from the dictionary
-print("Hostname is " + str(host_info_dictionary['hostnames']))
+print("Hostname is:\t" + str(hinfd['hostnames']) + "\nIP address is:\t" + str(hinfd['ip_str']) + "\nPorts:\t" + str(hinfd['ports']) + "\nOperating system:\t" + str(hinfd['os']) + "\nLocation\t" + str(hinfd['country_code']) + "8asn:\t" + str(hinfd['asn']) + "\nTags:\t"+ str(hinfd['tags']) + "\nLast update :\t" + str(hinfd['last_update']))
+
+if 'vulns' not in hinfd:
+    print("No vulns found")
+else:
+    print("\nVulnerabilities\t" + str(hinfd['vulns']))
+
+if (args.format) == 'text':
+    print(response.text)
+elif (args.format) == 'json':
+    print(host_info_json)
+
+
