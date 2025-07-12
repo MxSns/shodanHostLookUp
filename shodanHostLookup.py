9521e22890d24d2a15433b5550e65758221b4ca1
MxSns <maxime.sn210@gmail.com>
2025-07-13 01:05:41 +0200

    Adding an output option

--- shodanHostLookup.py
+++ shodanHostLookup.py
@@ -1,15 +1,16 @@
-import json, requests, argparse
+import json, requests, argparse, os
 
 parser = argparse.ArgumentParser(prog='shodanHostLookup', description='This script returns the shodan API result for an IP, turn it to json and print it')
 
 parser.add_argument('host', help='The IP address of the target')
 parser.add_argument('-k', '--key', required=True, help='Shodan\'s API key')
 parser.add_argument('-f', '--format', help='Format to display the datas text / json')
+parser.add_argument('-o', '--output', help='The path for the output file')
 
 args = parser.parse_args()
 
 
-# Get an API key from shodan and include it here
+# Variables
 api_key = (args.key)
 
 # Lookup an IP using shodan's REST API
@@ -22,6 +23,15 @@ hinfd = json.loads(response.text)
 # Format the output (indent, sort keys and don't convert Unicode) for printing JSON
 host_info_json = json.dumps(hinfd, indent=4, sort_keys=True, ensure_ascii=False)
 # print(host_info_json)
+
+# Create the path and dir for the output
+
+if args.output:
+     
+    output_file = os.path.join(args.output)
+    with open(output_file, 'w') as f:
+        f.write(host_info_json)
+    print(f"Results saved to {output_file}")
 
 # Print the hostname from the dictionary
 print("Hostname is:\t" + str(hinfd['hostnames']) + "\nIP address is:\t" + str(hinfd['ip_str']) + "\nPorts:\t" + str(hinfd['ports']) + "\nOperating system:\t" + str(hinfd['os']) + "\nLocation\t" + str(hinfd['country_code']) + "8asn:\t" + str(hinfd['asn']) + "\nTags:\t"+ str(hinfd['tags']) + "\nLast update :\t" + str(hinfd['last_update']))
